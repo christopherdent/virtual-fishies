abfe3b6f8484515cbc16b9c7c807188acaf4d784
= <christopher.dent@opscompass.com>
2024-02-07 16:43:48 -0500

    first commit

---
+++ src/Fish.vue
@@ -0,0 +1,11 @@
+<script setup>
+import GoldenPurpleFish from './public/golden-purple-fish.png'
+</script>
+
+<template>
+    <div>
+        <img :src="GoldenPurpleFish" alt="Golden Purple Fish" />
+    </div>
+</template>
+
+<style scoped></style>
